db3db2bab4ce80a9b7668a27f7ef2d62bc424eae
Tim Eaton <tim@yakdrive.io>
2025-02-01 18:53:25 +0100

    feat: added two new plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,20 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
+import expressiveCode from "astro-expressive-code";
+
+import playformCompress from "@playform/compress";
+
 export default defineConfig({
   site: "https://www.timeaton.dev/",
   author: "Tim Eaton",
   base: "/",
-  integrations: [sitemap(), mdx()],
+  integrations: [
+    sitemap(),
+    mdx(),
+    expressiveCode(),
+    playformCompress(),
+  ],
   markdown: {
     shikiConfig: {
       theme: "material-theme-darker",
